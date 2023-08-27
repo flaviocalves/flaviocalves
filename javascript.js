0852a518430457aafe73466bfb443ae54c59ece6
Flávio Cantanhede Alves <46876626+flaviocalves@users.noreply.github.com>
2023-08-27 17:32:38 -0300

    Iniciando o javascript

---
+++ javascript.js
@@ -0,0 +1,19 @@
+/*var nNome = 'Flavio';
+console.log(nNome);
+
+let nNome2 = 'Flavio2';
+console.log(nNome2);
+
+const nNome3 = 'Flavio3';
+console.log(nNome3);
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal();*/
+
+alert("Sejá bem vindo ao meu Profile");
